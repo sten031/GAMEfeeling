f641b3ab2aac5069716cc83255a7ae432d2db18a
shingo tanaka <stenstenktensten@gmail.com>
2020-04-07 07:14:00 +0000

    ログイン機能作成完了（レイアウト除く）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+
+
+    protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_japanese, :first_name_japanese, :last_name_katakana, :first_name_katakana, :postal_code, :address, :tel])
+  end
 end
