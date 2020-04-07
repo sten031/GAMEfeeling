26d6cb4f7ea103a3ffbcd571472ada084e9d5010
shingo tanaka <stenstenktensten@gmail.com>
2020-04-07 12:31:07 +0000

    投稿model,controller,view雛形作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_093030) do
+ActiveRecord::Schema.define(version: 2020_04_07_103935) do
+
+  create_table "games", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "feeling"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
