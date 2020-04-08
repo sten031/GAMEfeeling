0a61653911113eb41559174b00e7a04740f4e1c0
shingo tanaka <stenstenktensten@gmail.com>
2020-04-08 11:21:55 +0000

    投稿一覧、新規作成、詳細、編集機能作成

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,41 @@
 class GamesController < ApplicationController
   def new
+    @game = Game.new
   end
+
   def create
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+    @game.save
+    redirect_to games_path(@game.id)
   end
+
   def index
+    @games = Game.all
   end
+  
+  def edit
+    @game = Game.find(params[:id])
+  end
+
   def show
+    @game = Game.find(params[:id])
   end
+
   def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    redirect_to game_path(@game)
   end
+
   def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_path
   end
+
   private
-  def game_params#アクション名、メソッド
-    params.require(:game).permit( :title, :image, :feeling)
+  def game_params #アクション名、メソッド
+    params.require(:game).permit(:title, :image, :feeling)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_065243) do
+ActiveRecord::Schema.define(version: 2020_04_08_091943) do
 
   create_table "games", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.text "feeling"
-    t.string "image"
+    t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
